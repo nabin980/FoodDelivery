d73631b6da72779fb8a7eba76257d90d9718eae8
Nabin Bohara <olinaveen98@gmail.com>
2024-08-27 01:12:32 +0545

    some improvement

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,27 +1,38 @@
-import React, { useContext} from "react";
+import React, { useContext, useState } from "react";
 import "./FoodItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-
-const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="food-item">
       <div className="food-item-image-container">
         <img src={image} alt="" className="food-item-image" />
         {!cartItems[id] ? (
-          <img
+          // <img onClick={() => addToCart(id)} className="add" src={assets.add_icon_white}/>
+          <FontAwesomeIcon
             className="add"
+            icon={faCartShopping}
+            bounce={isHovered}
+            style={{ borderRadius: "50%",padding: "10px", color: "#ff0000", cursor: "pointer",backgroundColor: "#ffffff", }}
             onClick={() => addToCart(id)}
-            src={assets.add_icon_white}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
           />
         ) : (
           <div className="food-item-counter">
-            <img onClick={() => removeFromCart(id)}  src={assets.remove_icon_red} alt="" />
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
             <p>{cartItems[id]}</p>
-            <img onClick={() =>addToCart(id)} src={assets.add_icon_green}/>
+            <img onClick={() => addToCart(id)} src={assets.add_icon_green} />
           </div>
         )}
       </div>
